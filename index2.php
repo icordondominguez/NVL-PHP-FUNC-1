3d7a5e2775e6b191d09153dc4d85205a12fb27fa
Ismael Cordon Dominguez <icordondominguez@gmail.com>
2019-12-27 18:48:00 +0100

    modificamos la funcionalidad para que se busque la letra que no indiquen

--- index2.php
+++ index2.php
@@ -1,9 +1,10 @@
 <?php
 
-	$texto = "PHP me esta volviendo loca la cabeza";
-	cuentaLetraA($texto);
+	$texto = "Upgrade Hub";
+	$letra = "u";
+	cuentaLetraA($texto,$letra);
 
-	function cuentaLetraA($texto)
+	function cuentaLetraA($texto,$letra)
 	{
 		// creamos un contador para contar la cantidad de letras A que hay
 		$cont = 0;
@@ -11,16 +12,16 @@
 		// ponemos la cadena en minusculas por si nos meten alguna mayuscula
 		$texto = strtolower($texto);
 
-		// transformamos la cadena en array para poder buscar la letra A
+		// transformamos la cadena en array para poder buscar la letra que nos indiquen
 		$arr1 = str_split($texto);
 
-		// recorremos el array y comprobamos en cada posicion si hay una A, si lo hay sumamos 1 al contador
+		// recorremos el array y comprobamos en cada posicion si hay una letra como la que nos piden, si lo hay sumamos 1 al contador
 		foreach ($arr1 as $d) {
-			if($d == 'h'){
+			if($d == $letra){
 				$cont++;
 			}
 		}
-		echo "La cadena de texto " . "'$texto'" . " tiene " . $cont . " letras A";
+		echo "La cadena de texto " . "'$texto'" . " tiene " . $cont . " letras " . $letra;
 	}
-	
+
 ?>
